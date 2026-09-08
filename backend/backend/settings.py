6985dd55498ad521528d0484bc9b3244524c79fb
Evgenij Ivanov <jelsiv@yandex.ru>
2026-09-08 18:06:24 +0300

    убрано переопределение разрешенных хостов

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -8,7 +8,7 @@ SECRET_KEY = 'django-insecure-j_89af+30&&4qm*8z9_(^zz8p4-ho8z_m6ylm0s$h!-p@on1_^
 
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '158.160.196.44', 'taski-24.duckdns.org']
 
 
 # Application definition
@@ -119,5 +119,3 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CORS_ORIGIN_WHITELIST = [
     'http://localhost:3000'
 ]
-
-ALLOWED_HOSTS = ['localhost', '158.160.196.44', 'taski-24.duckdns.org', '127.0.0.1']
